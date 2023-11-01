5d7ff1cb009be9c753eb179b21b20c993d107108
Angua1207 <alexis.bodson@student.hepl.be>
2023-11-01 13:04:05 +0100

    Client Java/C fini

--- src/GUI/WindowClient.java
+++ src/GUI/WindowClient.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class WindowClient extends JFrame {
     private JPanel contentPane;
@@ -23,20 +24,23 @@ public class WindowClient extends JFrame {
     private JButton acheterButton;
     private JPanel Publiciter;
     private JPanel JpanelPanier;
-    private JTable Panier;
+    private JTable tablePanier;
     private JButton supprimerArticleButton;
-    private JButton vidéeLePanierButton;
-    private JButton confirmérAchatButton;
+    private JButton viderLePanierButton;
+    private JButton confirmerButton;
     private JLabel Image;
     private JTextField textFieldTotal;
     private JButton buttonOK;
     private JButton buttonCancel;
 
-    private int idClient;
-    private int idArticleEnCours;
-    private TcpConnection serveurConnexion;
-
+    private final TcpConnection serverConnection;
+    private int articleEnCours;
+    private int nbArticlePanier;
+    private String nomclient;
     public WindowClient() {
+
+        articleEnCours = 1;
+
 
 
         setContentPane(contentPane);
@@ -46,19 +50,28 @@ public class WindowClient extends JFrame {
         // Centrer la fenêtre
         setLocationRelativeTo(null);
         pack();
-        DefaultTableModel model = (DefaultTableModel) Panier.getModel(); // Récupérez le modèle de la JTable
+        setSize(750,600); // le pack() etire fort la table donc on fixe la taille pour que se soit plus jolie
 
-        model.setColumnIdentifiers(new String[]{"Articles","Prix à l'unité","Quantité"});
-        Panier.setModel(model);
+        // changer les proprietes de la JTable
+        tablePanier.setDefaultEditor(Object.class, null);
+        tablePanier.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tablePanier.setRowSelectionAllowed(true);
+        tablePanier.setColumnSelectionAllowed(false);
+
+        /*DefaultTableModel model = new DefaultTableModel();
+        model.setColumnIdentifiers(new String[]{"Article", "Prix à l'unité", "Quantité"});
+        tablePanier.setModel(model);*/
         textFieldTotal.setText("0.00");
+
+        logoutok();
 
         /******CONNEXION SERVEUR******/
 
-      /*  try {
-            serveurConnexion = new TcpConnection("192.168.226.128",50000);
+        try {
+            serverConnection = new TcpConnection("192.168.157.128", 50000);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }*/
+        }
 
 
         /****************FERMERFENETRE*******************/
@@ -67,9 +80,9 @@ public class WindowClient extends JFrame {
 
             @Override
             public void windowClosing(WindowEvent e) {
-
+                OVESP_CancelAll();
                 try {
-                    serveurConnexion.Close();
+                    serverConnection.Close();
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 }
@@ -114,57 +127,86 @@ public class WindowClient extends JFrame {
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if (OVESP_Login())
+                    OVESP_Consult(articleEnCours);
 
                 loginok();
+                nomclient = textFieldNom.getText();
 
             }
         });
         logoutButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                try {
-                    serveurConnexion.Close();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+                OVESP_CancelAll();
+                viderCaddie();
+                articleEnCours = 1;
+                OVESP_Consult(articleEnCours);
                 logoutok();
             }
         });
         buttonPrevious.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if(articleEnCours > 1)
+                    OVESP_Consult(articleEnCours - 1);
 
             }
         });
         buttonNext.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if(articleEnCours < 21)
+                    OVESP_Consult(articleEnCours + 1);
 
             }
         });
         acheterButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if (spinnerQantite.getValue().equals(0)){
+                    JOptionPane.showMessageDialog(null, "Entrez une quantité", "Erreur d'achat", JOptionPane.ERROR_MESSAGE);
+                }
+                if (nbArticlePanier >=10){
+                    JOptionPane.showMessageDialog(null, "Pannier pleins", "Erreur d'achat", JOptionPane.ERROR_MESSAGE);
+
+                }
+                else {
+                    if (OVESP_Achat()){
+                        OVESP_Caddie();
+                        OVESP_Consult(articleEnCours);
+                    }
+                }
 
             }
         });
         supprimerArticleButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if (tablePanier.getSelectedRow() == -1)
+                    JOptionPane.showMessageDialog(null, "Veuillez sélectionner un article", "Erreur de suppression", JOptionPane.ERROR_MESSAGE);
+                else{
+                    OVESP_Cancel();
+                    OVESP_Caddie();
+                    OVESP_Consult(articleEnCours);
+                }
 
             }
         });
-        vidéeLePanierButton.addActionListener(new ActionListener() {
+        viderLePanierButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                OVESP_CancelAll();
+                OVESP_Caddie();
+                OVESP_Consult(articleEnCours);
 
             }
         });
-        confirmérAchatButton.addActionListener(new ActionListener() {
+        confirmerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                OVESP_Confirmer();
+                viderCaddie();
             }
         });
 
@@ -188,8 +230,8 @@ public class WindowClient extends JFrame {
 
         //Panier
         supprimerArticleButton.setEnabled(true);
-        vidéeLePanierButton.setEnabled(true);
-        confirmérAchatButton.setEnabled(true);
+        viderLePanierButton.setEnabled(true);
+        confirmerButton.setEnabled(true);
 
     }
 
@@ -211,16 +253,267 @@ public class WindowClient extends JFrame {
         //Panier
 
         supprimerArticleButton.setEnabled(false);
-        vidéeLePanierButton.setEnabled(false);
-        confirmérAchatButton.setEnabled(false);
+        viderLePanierButton.setEnabled(false);
+        confirmerButton.setEnabled(false);
 
     }
 
-    public void videeCaddie()
-    {
+    /**
+     * Permet d'afficher un article sur le GUI
+     * @param nom Nom de l'article
+     * @param stock Quantité de l'article
+     * @param prix Prix de l'article
+     * @param image Nom du fichier de l'article ("src/Images/" sera ajouté dans la fonction)
+     */
+    public void setArticle(String nom, String stock, String prix, String image){
+        textFieldArticle.setText(nom);
+        textFieldStock.setText(stock);
+        textFieldPrixunit.setText(prix);
+        Image.setIcon(new ImageIcon("src/GUI/Images/" + image));
+    }
 
+    /**
+     * Permet de vider le panier et mettre à 0 le prix dans le GUI
+     */
+    public void viderCaddie(){
+        DefaultTableModel model = new DefaultTableModel();
+        model.setColumnIdentifiers(new String[]{"Article", "Prix à l'unité", "Quantité"});
+        tablePanier.setModel(model);
+        textFieldTotal.setText("0.00");
+    }
+
+    //OVESP
+    //********** Fonction de protocol OVESP **********************************
+    /**
+     * Connecte le client à son compte Vérification de l’existence et du mot de passe du client / Création d’un nouveau client dans la table clients
+     */
+    public boolean OVESP_Login(){
+        String data;
+        String response;
+
+        if (textFieldNom.getText().isEmpty() || textFieldMdp.getText().isEmpty()){// si un des deux champs est vide
+            JOptionPane.showMessageDialog(null, "Remplisez les champs !", "Erreur de connection", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        // Envoie de la requete
+        int value = nouveauClientCheckBox.isSelected() ? 1 : 0; // 1 = true = coche // 0 = false = pas coche
+        data = "LOGIN#" + value + "#" + textFieldNom.getText() + "#" + textFieldMdp.getText();
+        try {
+            serverConnection.Send(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Reception et parsing de la reponse
+        try {
+            response = serverConnection.Receive();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] champs = response.split("#");
+
+        System.out.println("DEBUG : " + champs[1]);
+
+        if (champs[1].equals("OK")){
+            JOptionPane.showMessageDialog(null, champs[2], "Connection réussie !", JOptionPane.INFORMATION_MESSAGE);
+            loginok();
+            return true;
+        }
+        else {
+            JOptionPane.showMessageDialog(null, champs[2], "Erreur de connection", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
 
     }
+    public void OVESP_Consult(int article){
+        String data;
+        String response;
+
+        // Envoie de la requete
+        data = "CONSULT#" + article;
+        try {
+            serverConnection.Send(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Reception et parsing de la reponse
+        try {
+            response = serverConnection.Receive();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] champs = response.split("#");
+        int id = Integer.parseInt(champs[1]);
+        if (id > 0)
+        {
+            articleEnCours = id;
+            System.out.println("DEBUG : " + champs[1]);
+            System.out.println("DEBUG : " + champs[2]);
+            System.out.println("DEBUG : " + champs[3]);
+            System.out.println("DEBUG : " + champs[4]);
+            System.out.println("DEBUG : " + champs[5]);
+
+            setArticle(champs[2],champs[4],champs[3],champs[5]);
+        }
+    }
+    public boolean OVESP_Achat() {
+        String data;
+        String response;
+        int quantite = (int) spinnerQantite.getValue();
+        data = "ACHAT#" + articleEnCours + "#" + quantite;
+
+
+        try {
+            serverConnection.Send(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Reception et parsing de la reponse
+        try {
+            response = serverConnection.Receive();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] champs = response.split("#");
+        if (Integer.parseInt(champs[1]) <= 0) {
+            switch (Integer.parseInt(champs[1])) {
+                case 0 -> JOptionPane.showMessageDialog(null, "Stock insufisant", "Erreur d'achat", JOptionPane.ERROR_MESSAGE);
+                case -1 -> JOptionPane.showMessageDialog(null, "Article non trouvé", "Erreur d'achat", JOptionPane.ERROR_MESSAGE);
+            }
+            return false;
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Achat réussie", "Achat réussie", JOptionPane.INFORMATION_MESSAGE);
+            return true;
+        }
+    }
+
+    public void OVESP_Caddie(){
+        String data;
+        String response;
+        int nbArticle;
+        double prixTotal = 0;
+
+        // Envoie de la requete
+        data = "CADDIE" ;
+        try {
+            serverConnection.Send(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Reception et parsing de la reponse
+        try {
+            response = serverConnection.Receive();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] champs = response.split("#");
+
+        nbArticlePanier = Integer.parseInt(champs[1]);
+        System.out.println("DEBUG : " + champs[1]);
+
+
+
+        DefaultTableModel model = new DefaultTableModel();
+        model.setColumnIdentifiers(new String[]{"Article", "Prix à l'unité", "Quantité"});
+
+        for (int i = 0; i<nbArticlePanier; i++){
+            // Définir les noms des colonnes
+            System.out.println("DEBUG : " + champs[(i*4)+3]);
+            System.out.println("DEBUG : " + champs[(i*4)+5]);
+            System.out.println("DEBUG : " + champs[(i*4)+4]);
+
+            model.addRow(new Object[]{champs[(i*4)+3], champs[(i*4)+5], champs[(i*4)+4]});
+            prixTotal += Float.parseFloat(champs[(i*4)+5]) * Integer.parseInt(champs[(i*4)+4]);
+        }
+
+
+        // Formater le prix avec deux chiffres après la virgule
+        DecimalFormat df = new DecimalFormat("#.##");
+
+
+
+        textFieldTotal.setText(df.format(prixTotal));
+        tablePanier.setModel(model);
+
+    }
+    public void OVESP_Cancel() {
+        String data;
+        String response;
+        data = "CANCEL#" + tablePanier.getSelectedRow()+"#"+nbArticlePanier;
+        try {
+            serverConnection.Send(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Reception et parsing de la reponse
+        try {
+            response = serverConnection.Receive();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] champs = response.split("#");
+        if (! champs[1].equals("OK"))
+            JOptionPane.showMessageDialog(null, "Erreur durant la suppresion de l'article", "Erreur de suppression", JOptionPane.ERROR_MESSAGE);
+
+    }
+    public void OVESP_CancelAll(){
+        String data;
+        String response;
+        data = "CANCELALL#" +nbArticlePanier;
+        try {
+            serverConnection.Send(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Reception et parsing de la reponse
+        try {
+            response = serverConnection.Receive();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] champs = response.split("#");
+        if (! champs[1].equals("OK"))
+            JOptionPane.showMessageDialog(null, "Erreur durant la suppresion du panier", "Erreur de suppression", JOptionPane.ERROR_MESSAGE);
+
+    }
+    public void OVESP_Confirmer(){
+        String data;
+        String response;
+
+        String prixt = textFieldTotal.getText();
+        prixt = prixt.replace(',', '.');
+        float prixtotal = Float.parseFloat(prixt);
+
+        data = "CONFIRMER#"+ nomclient + "#" + prixtotal + "#" + nbArticlePanier;
+        try {
+            serverConnection.Send(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Reception et parsing de la reponse
+        try {
+            response = serverConnection.Receive();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] champs = response.split("#");
+        int numfac = Integer.parseInt(champs[1]);
+        if(numfac > 0)
+            JOptionPane.showMessageDialog(null, "La commande à bien été envoyé au Maraîcher. Numéro de facture : " + champs[1], "Commande réussi", JOptionPane.INFORMATION_MESSAGE);
+        else
+            JOptionPane.showMessageDialog(null, "Une erreur est survenue lors du passage de la commande", "Erreur de commande", JOptionPane.ERROR_MESSAGE);
+
+    }
+
+
 
 
 
